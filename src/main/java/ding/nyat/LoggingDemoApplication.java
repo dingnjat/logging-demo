4bb9e85b48da9ec482de31296df0da5520379ac2
dingnjat <annanshakai@gmail.com>
2019-08-19 02:27:32 +0700

    async logger

--- src/main/java/ding/nyat/LoggingDemoApplication.java
+++ src/main/java/ding/nyat/LoggingDemoApplication.java
@@ -6,6 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LoggingDemoApplication {
 
+    static {
+        System.setProperty("Log4jContextSelector", "org.apache.logging.log4j.core.async.AsyncLoggerContextSelector");
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(LoggingDemoApplication.class, args);
     }
