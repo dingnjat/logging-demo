12316d6b8accbf2c67d51653669f39db89c45233
dingnjat <annanshakai@gmail.com>
2019-08-19 03:55:29 +0700

    config by spring profile

--- src/main/java/ding/nyat/controller/WebController.java
+++ src/main/java/ding/nyat/controller/WebController.java
@@ -1,17 +1,18 @@
 package ding.nyat.controller;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class WebController {
-    private Logger logger = LogManager.getLogger(WebController.class);
+
+    @Value("${test.value}")
+    private String value;
 
     @GetMapping("/")
     public String home() {
-        logger.warn("HOHO");
-        return "indexx";
+        System.out.println(value);
+        return "index";
     }
 }
